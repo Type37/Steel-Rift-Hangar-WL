837cd468e6a477beb4da004394eed8dc3503d0df
Jet Wong <68975087+Type37@users.noreply.github.com>
2026-04-28 14:26:18 -0400

    Update las-wing-attack-squadron.js to 1.5 rules (Ch16: stats, traits, Asset Command, Auxiliary Unit)

--- src/data/support-assets/las-wing-attack-squadron.js
+++ src/data/support-assets/las-wing-attack-squadron.js
@@ -1,11 +1,14 @@
 import {VEH_AGM_MISSILES, VEH_BARRAGE_ROCKETS, VEH_LAS_AUTO_CANNON, VEH_ROTARY_CANNON} from '../unit-weapons.js';
 import {
+    TRAIT_AUXILIARY_UNIT,
     TRAIT_FLYING,
     TRAIT_FLYING_SQUADRON,
-    TRAIT_MINE_SWEEPER,
+    TRAIT_GUIDANCE_SUITE,
     TRAIT_MSOE_LAUNCHER,
     TRAIT_SCRAMBLERS,
     TRAIT_UNIT_SIZE_AND_TYPE,
+    TRAIT_VULNERABLE,
+    TRAIT_YIELDING,
 } from '../unit-traits.js';
 import {SIZE_LIGHT} from '../unit-sizes.js';
 import {makeStaticListIds, trait} from '../data-helpers.js';
@@ -15,8 +18,8 @@ export const LAS_WING_ATTACK_SQUADRON = 'LAS_WING_ATTACK_SQUADRON';
 
 const baseStats = {
     move: 12,
-    armor: 2,
-    structure: 0,
+    armor: 1,
+    structure: 1,
 };
 
 export const LAS_WING_ATTACK_SQUADRON_DATA = {
@@ -29,8 +32,11 @@ export const LAS_WING_ATTACK_SQUADRON_DATA = {
         defense: 3,
         traits: [
             trait(TRAIT_UNIT_SIZE_AND_TYPE, null, 'Light Vehicle'),
+            trait(TRAIT_AUXILIARY_UNIT, null, 'Light'),
             trait(TRAIT_FLYING),
             trait(TRAIT_FLYING_SQUADRON),
+            trait(TRAIT_VULNERABLE),
+            trait(TRAIT_YIELDING),
         ],
         vehicles: makeStaticListIds({
             STRIKE_LAS_WING: {
@@ -58,8 +64,8 @@ export const LAS_WING_ATTACK_SQUADRON_DATA = {
                     ],
                 },
                 traits: [
-                    trait(TRAIT_MINE_SWEEPER),
-                    trait(TRAIT_MSOE_LAUNCHER),
+                    trait(TRAIT_GUIDANCE_SUITE, null, 'Flying Move'),
+                    trait(TRAIT_MSOE_LAUNCHER, null, 'Flying Move'),
                     trait(TRAIT_SCRAMBLERS),
                 ],
             },
